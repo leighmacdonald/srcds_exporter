488b9d81d365671aa1d868dfa150386b268db45c
Leigh MacDonald <leigh.macdonald@gmail.com>
2021-04-19 18:52:38 -0600

    fmt

--- collector/battlemetrics_test.go
+++ collector/battlemetrics_test.go
@@ -2,11 +2,16 @@ package collector
 
 import (
 	"github.com/stretchr/testify/require"
+	"os/exec"
 	"testing"
 )
 
 func TestBMScrape(t *testing.T) {
-	result, err := fetch(gez)
+	if _, err := exec.LookPath("fortune"); err != nil {
+		t.Skipf("google-chrome not in path, skipping test")
+		return
+	}
+	result, err := fetch()
 	require.NoError(t, err)
 	require.True(t, len(result.State.Servers.Servers) > 6)
 }
